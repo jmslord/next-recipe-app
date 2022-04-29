8255218ede9b854bcf568fe47e778e6c3a53752b
jmslord <jmseffendy@gmail.com>
2022-04-29 12:43:29 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <nav className="header">
+      <div>
+        <Link href="/">
+        <a>James's Kitchen</a>
+        </Link>
+      </div>
+    </nav>
+    <main>
+    <Component {...pageProps} />
+    </main>
+    </>
+  );
+  
 }
 
 export default MyApp
